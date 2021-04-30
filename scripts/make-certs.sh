1bea9cb24d93eb5bfa502c950b0c557769c64dfc
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-04-30 16:05:55 +0200

    rollback tls

--- scripts/make-certs.sh
+++ scripts/make-certs.sh
@@ -2,12 +2,6 @@
 
 mkdir -p certs
 
-# TLS spidtest-env2
-openssl req -nodes -new -x509 -sha256 -days 365 -newkey rsa:2048 \
-    -config scripts/tls.conf \
-    -keyout certs/certificate.pem -out certs/certificate.crt
-
-# METADATA CERT
 openssl req -nodes -new -x509 -sha256 -days 365 -newkey rsa:2048 \
     -subj "/C=IT/ST=State/L=City/O=Acme Inc. /OU=IT Department/CN=spid-express.selfsigned.example" \
     -keyout certs/key.pem -out certs/cert.pem

6f9b62a2b84ed2b57ab4c5fd7255e04ef8d87635
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-04-30 16:16:09 +0200

    Fix comment

--- scripts/make-certs.sh
+++ scripts/make-certs.sh
@@ -7,7 +7,7 @@ openssl req -nodes -new -x509 -sha256 -days 365 -newkey rsa:2048 \
     -config scripts/tls.conf \
     -keyout certs/certificate.pem -out certs/certificate.crt
 
-# METADATA CERT
+# TLS METADATA ENDPOINT hub-spid-login-ms
 openssl req -nodes -new -x509 -sha256 -days 365 -newkey rsa:2048 \
-    -subj "/C=IT/ST=State/L=City/O=Acme Inc. /OU=IT Department/CN=spid-express.selfsigned.example" \
+    -subj "/C=IT/ST=State/L=City/O=Acme Inc. /OU=IT Department/CN=hub-spid-login-ms" \
     -keyout certs/key.pem -out certs/cert.pem

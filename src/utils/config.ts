7b35d7786bb76b09270ecdcbf64fcb77729c670b
Ivan Diana <iwo.diana@gmail.com>
2021-12-10 13:45:52 +0100

    added JWT_TOKEN_KID as optional when jwt  enabled

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -127,14 +127,17 @@ const JWTParams = t.union([
     t.interface({
       ENABLE_JWT: t.literal(true),
       JWT_TOKEN_ISSUER: NonEmptyString,
-      JWT_TOKEN_PRIVATE_KEY: NonEmptyString
+      JWT_TOKEN_PRIVATE_KEY: NonEmptyString,
     }),
-    UserRegistryParams
+    t.partial({
+      JWT_TOKEN_KID: NonEmptyString
+    }),
+    UserRegistryParams,
   ]),
   t.interface({
     ENABLE_JWT: t.literal(false),
-    ENABLE_USER_REGISTRY: t.literal(false)
-  })
+    ENABLE_USER_REGISTRY: t.literal(false),
+  }),
 ]);
 type JWTParams = t.TypeOf<typeof JWTParams>;
 

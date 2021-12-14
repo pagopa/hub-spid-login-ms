58033e0c263b483fb1838883c3185744b768cb2a
Ivan Diana <iwo.diana@gmail.com>
2021-12-14 08:29:47 +0100

    remove unuseful field type declaration

--- src/utils/__tests__/jwtparams.test.ts
+++ src/utils/__tests__/jwtparams.test.ts
@@ -1,4 +1,3 @@
-import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { JWTParams } from "../config";
 
 describe("Validate an unknown type as JWTParams Interface", () => {
@@ -17,9 +16,9 @@ describe("Validate an unknown type as JWTParams Interface", () => {
   it("should validate a correct JWT Param", () => {
     const JWTParamMock: unknown = {
       ENABLE_JWT: true,
-      JWT_TOKEN_ISSUER: "issuer" as NonEmptyString,
-      JWT_TOKEN_PRIVATE_KEY: "tokenprivatekey" as NonEmptyString,
-      JWT_TOKEN_KID: "tokenkid" as NonEmptyString,
+      JWT_TOKEN_ISSUER: "issuer",
+      JWT_TOKEN_PRIVATE_KEY: "tokenprivatekey",
+      JWT_TOKEN_KID: "tokenkid",
       ENABLE_USER_REGISTRY: false,
     };
 
@@ -30,7 +29,7 @@ describe("Validate an unknown type as JWTParams Interface", () => {
   it("should not validate an incorrect incoming JWT Param", () => {
     const JWTParamMock: unknown = {
       ENABLE_JWT: false,
-      JWT_TOKEN_KID: "tokenkid" as NonEmptyString,
+      JWT_TOKEN_KID: "tokenkid",
     };
 
     const value = JWTParams.decode(JWTParamMock);
@@ -40,7 +39,7 @@ describe("Validate an unknown type as JWTParams Interface", () => {
   it("should not validate an incorrect incoming JWT Param", () => {
     const JWTParamMock: unknown = {
       ENABLE_JWT: true,
-      JWT_TOKEN_KID: "tokenkid" as NonEmptyString,
+      JWT_TOKEN_KID: "tokenkid",
     };
 
     const value = JWTParams.decode(JWTParamMock);

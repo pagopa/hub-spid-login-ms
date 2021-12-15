4bc3d6deb6c33a8d9d9d6f9501c1c4742a50e550
Michael Disaro <michael.disaro@gmail.com>
2021-12-15 14:19:12 +0100

    Relaxed organization_fiscal_code to be a WithinRangeString instead of OrganizationFiscalCode

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,9 +1,9 @@
-import { OrganizationFiscalCode } from "@pagopa/ts-commons/lib/strings";
+import { WithinRangeString } from "@pagopa/ts-commons/lib/strings";
 import * as t from "io-ts";
 
 export const UpgradeTokenBody = t.exact(
   t.interface({
-    organization_fiscal_code: OrganizationFiscalCode
+    organization_fiscal_code: WithinRangeString(8, 16),
   })
 );
 export type UpgradeTokenBody = t.TypeOf<typeof UpgradeTokenBody>;

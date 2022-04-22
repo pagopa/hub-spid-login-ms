e73301eafffefe42431c6d17e0ebb9614a3954cc
Emanuele De Cupis <ema.decu@gmail.com>
2022-04-22 15:52:44 +0200

    refactor: src/utils/config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -13,9 +13,9 @@ import {
   EmailString,
   OrganizationFiscalCode,
 } from "@pagopa/ts-commons/lib/strings";
-import { fromNullable as fromNullableE } from "fp-ts/lib/Either";
-import { identity } from "fp-ts/lib/function";
-import { fromNullable } from "fp-ts/lib/Option";
+import * as E from "fp-ts/lib/Either";
+import * as O from "fp-ts/lib/Option";
+import { pipe, flow, identity } from "fp-ts/lib/function";
 import * as t from "io-ts";
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
@@ -179,54 +179,88 @@ const DEFAULT_SERVER_PORT = 8080;
 // No need to re-evaluate this object for each call
 const errorOrConfig: t.Validation<IConfig> = IConfig.decode({
   ...process.env,
-  ALLOW_CORS: fromNullable(process.env.ALLOW_CORS)
-    .map((_) => _.toLowerCase() === "true")
-    .getOrElseL(() => false),
-  APPINSIGHTS_DISABLED: fromNullable(process.env.APPINSIGHTS_DISABLED)
-    .map((_) => _.toLowerCase() === "true")
-    .getOrElseL(() => true),
-  ENABLE_ADE_AA: fromNullable(process.env.ENABLE_ADE_AA)
-    .map((_) => _.toLowerCase() === "true")
-    .getOrElseL(() => false),
-  ENABLE_FULL_OPERATOR_METADATA: fromNullable(
-    process.env.ENABLE_FULL_OPERATOR_METADATA
-  )
-    .map((_) => _.toLowerCase() === "true")
-    .getOrElseL(() => false),
-  ENABLE_JWT: fromNullable(process.env.ENABLE_JWT)
-    .map((_) => _.toLowerCase() === "true")
-    .getOrElseL(() => false),
-  ENABLE_SPID_ACCESS_LOGS: fromNullable(process.env.ENABLE_SPID_ACCESS_LOGS)
-    .map((_) => _.toLowerCase() === "true")
-    .getOrElseL(() => false),
-  ENABLE_USER_REGISTRY: fromNullable(process.env.ENABLE_USER_REGISTRY)
-    .map((_) => _.toLowerCase() === "true")
-    .getOrElseL(() => false),
-  INCLUDE_SPID_USER_ON_INTROSPECTION: fromNullable(
-    process.env.INCLUDE_SPID_USER_ON_INTROSPECTION
-  )
-    .map((_) => _.toLowerCase() === "true")
-    .getOrElseL(() => false),
-  L1_TOKEN_EXPIRATION: IntegerFromString.decode(process.env.L1_TOKEN_EXPIRATION)
-    .mapLeft((_) => undefined)
-    .fold(identity, identity),
-  L2_TOKEN_EXPIRATION: IntegerFromString.decode(process.env.L2_TOKEN_EXPIRATION)
-    .mapLeft((_) => undefined)
-    .fold(identity, identity),
-  REDIS_CLUSTER_ENABLED: fromNullable(process.env.REDIS_CLUSTER_ENABLED)
-    .map((_) => _.toLowerCase() === "true")
-    .toUndefined(),
-  REDIS_TLS_ENABLED: fromNullable(process.env.REDIS_TLS_ENABLED)
-    .map((_) => _.toLowerCase() === "true")
-    .toUndefined(),
-  SERVER_PORT: fromNullableE(DEFAULT_SERVER_PORT)(process.env.SERVER_PORT)
-    .chain((_) =>
-      IntegerFromString.decode(_).mapLeft(() => DEFAULT_SERVER_PORT)
-    )
-    .fold(identity, identity),
-  TOKEN_EXPIRATION: fromNullableE(-1)(process.env.TOKEN_EXPIRATION)
-    .chain((_) => IntegerFromString.decode(_).mapLeft(() => -1))
-    .fold(() => 3600, identity),
+  ALLOW_CORS: pipe(
+    O.fromNullable(process.env.ALLOW_CORS),
+    O.map((_) => _.toLowerCase() === "true"),
+    O.getOrElse(() => false)
+  ),
+  APPINSIGHTS_DISABLED: pipe(
+    O.fromNullable(process.env.APPINSIGHTS_DISABLED),
+    O.map((_) => _.toLowerCase() === "true"),
+    O.getOrElse(() => true)
+  ),
+  ENABLE_ADE_AA: pipe(
+    O.fromNullable(process.env.ENABLE_ADE_AA),
+    O.map((_) => _.toLowerCase() === "true"),
+    O.getOrElse(() => false)
+  ),
+  ENABLE_FULL_OPERATOR_METADATA: pipe(
+    O.fromNullable(process.env.ENABLE_FULL_OPERATOR_METADATA),
+    O.map((_) => _.toLowerCase() === "true"),
+    O.getOrElse(() => false)
+  ),
+  ENABLE_JWT: pipe(
+    O.fromNullable(process.env.ENABLE_JWT),
+    O.map((_) => _.toLowerCase() === "true"),
+    O.getOrElse(() => false)
+  ),
+  ENABLE_SPID_ACCESS_LOGS: pipe(
+    O.fromNullable(process.env.ENABLE_SPID_ACCESS_LOGS),
+    O.map((_) => _.toLowerCase() === "true"),
+    O.getOrElse(() => false)
+  ),
+  ENABLE_USER_REGISTRY: pipe(
+    O.fromNullable(process.env.ENABLE_USER_REGISTRY),
+    O.map((_) => _.toLowerCase() === "true"),
+    O.getOrElse(() => false)
+  ),
+  INCLUDE_SPID_USER_ON_INTROSPECTION: pipe(
+    O.fromNullable(process.env.INCLUDE_SPID_USER_ON_INTROSPECTION),
+    O.map((_) => _.toLowerCase() === "true"),
+    O.getOrElse(() => false)
+  ),
+  L1_TOKEN_EXPIRATION: pipe(
+    process.env.L1_TOKEN_EXPIRATION,
+    IntegerFromString.decode,
+    E.mapLeft((_) => undefined),
+    E.fold(identity, identity)
+  ),
+  L2_TOKEN_EXPIRATION: pipe(
+    process.env.L2_TOKEN_EXPIRATION,
+    IntegerFromString.decode,
+    E.mapLeft((_) => undefined),
+    E.fold(identity, identity)
+  ),
+  REDIS_CLUSTER_ENABLED: pipe(
+    O.fromNullable(process.env.REDIS_CLUSTER_ENABLED),
+    O.map((_) => _.toLowerCase() === "true"),
+    O.getOrElseW(() => undefined) // O.toUndefined()
+  ),
+  REDIS_TLS_ENABLED: pipe(
+    O.fromNullable(process.env.REDIS_TLS_ENABLED),
+    O.map((_) => _.toLowerCase() === "true"),
+    O.getOrElseW(() => undefined) // O.toUndefined()
+  ),
+  SERVER_PORT: pipe(
+    E.fromNullable(DEFAULT_SERVER_PORT)(process.env.SERVER_PORT),
+    E.chain(
+      flow(
+        IntegerFromString.decode,
+        E.mapLeft(() => DEFAULT_SERVER_PORT)
+      )
+    ),
+    E.fold(identity, identity)
+  ),
+  TOKEN_EXPIRATION: pipe(
+    E.fromNullable(-1)(process.env.TOKEN_EXPIRATION),
+    E.chain(
+      flow(
+        IntegerFromString.decode,
+        E.mapLeft(() => -1)
+      )
+    ),
+    E.fold(() => 3600, identity)
+  ),
   isProduction: process.env.NODE_ENV === "prod",
 });
 
@@ -248,7 +282,10 @@ export function getConfig(): t.Validation<IConfig> {
  * @throws validation errors found while parsing the application configuration
  */
 export function getConfigOrThrow(): IConfig {
-  return errorOrConfig.getOrElseL((errors) => {
-    throw new Error(`Invalid configuration: ${readableReport(errors)}`);
-  });
+  return pipe(
+    errorOrConfig,
+    E.getOrElseW((errors) => {
+      throw new Error(`Invalid configuration: ${readableReport(errors)}`);
+    })
+  );
 }

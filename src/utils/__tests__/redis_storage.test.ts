56ad9b28eb614dfe45f60a8ca1c0a7b251954f36
LoFrance <lorenzo.franceschini@pagopa.it>
2022-04-23 11:57:03 +0200

    refactor: src/utils/__tests__/redis_storage.test.ts

--- src/utils/__tests__/redis_storage.test.ts
+++ src/utils/__tests__/redis_storage.test.ts
@@ -1,10 +1,16 @@
 // tslint:disable: no-any
-import { isNone } from "fp-ts/lib/Option";
+import { identity, pipe, flow } from "fp-ts/lib/function";
+
 import {
   existsKeyTask,
   getTask,
-  setWithExpirationTask
+  setWithExpirationTask,
 } from "../redis_storage";
+import * as T from "fp-ts/lib/Task";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as E from "fp-ts/lib/Either";
+import * as O from "fp-ts/lib/Option";
+import { boolean } from "fp-ts";
 
 const aRedisKey = "KEY";
 const aRedisValue = "VALUE";
@@ -18,125 +24,117 @@ const existsMock = jest.fn().mockImplementation((_, cb) => cb(null, 1));
 const redisClientMock = {
   exists: existsMock,
   get: getMock,
-  set: setMock
+  set: setMock,
 };
 
 describe("setWithExpirationTask", () => {
   it("should return true if redis store key-value pair correctly", async () => {
-    await setWithExpirationTask(
-      redisClientMock as any,
-      aRedisKey,
-      aRedisValue,
-      aRedisDefaultExpiration
-    )
-      .fold(
-        _ => fail(),
-        value => expect(value).toEqual(true)
-      )
-      .run();
+    const value = await pipe(
+      await setWithExpirationTask(
+        redisClientMock as any,
+        aRedisKey,
+        aRedisValue,
+        aRedisDefaultExpiration
+      ),
+      TE.fold(fail, T.of)
+    )();
+    expect(value).toEqual(true);
   });
 
   it("should return an error if redis store key-value pair returns undefined", async () => {
     setMock.mockImplementationOnce((_, __, ___, ____, cb) =>
       cb(undefined, undefined)
     );
-    await setWithExpirationTask(
-      redisClientMock as any,
-      aRedisKey,
-      aRedisValue,
-      aRedisDefaultExpiration
-    )
-      .fold(
-        _ => expect(_).toBeDefined(),
-        () => fail()
-      )
-      .run();
+    const value = pipe(
+      await setWithExpirationTask(
+        redisClientMock as any,
+        aRedisKey,
+        aRedisValue,
+        aRedisDefaultExpiration
+      ),
+      TE.fold(T.of, fail)
+    )();
+    expect(value).toBeDefined();
   });
 
   it("should return an error if redis store key-value pair fails", async () => {
     setMock.mockImplementationOnce((_, __, ___, ____, cb) =>
       cb(new Error("Cannot store key-value pair"), undefined)
     );
-    await setWithExpirationTask(
-      redisClientMock as any,
-      aRedisKey,
-      aRedisValue,
-      aRedisDefaultExpiration
-    )
-      .fold(
-        _ => expect(_).toBeDefined(),
-        () => fail()
-      )
-      .run();
+    const value = pipe(
+      await setWithExpirationTask(
+        redisClientMock as any,
+        aRedisKey,
+        aRedisValue,
+        aRedisDefaultExpiration
+      ),
+      TE.fold(T.of, fail)
+    )();
+    expect(value).toBeDefined();
   });
 });
 
 describe("getTask", () => {
   it("should return a value if redis get key-value pair correctly", async () => {
-    await getTask(redisClientMock as any, aRedisKey)
-      .fold(
-        () => fail(),
-        maybeResult =>
-          maybeResult.foldL(
-            () => fail(),
-            value => expect(value).toEqual(aRedisValue)
-          )
+    const res = await pipe(
+      TE.tryCatch(getTask(redisClientMock as any, aRedisKey), () => E.toError),
+      TE.fold(
+        () => fail,
+        (maybeResult) => flow(O.fromEither, O.fold(fail, T.of))(maybeResult)
       )
-      .run();
+    )();
+    if (O.isSome(res)) {
+      expect(res.value).toEqual(aRedisValue);
+    }
   });
 
   it("should return none if no value was found for the provided key", async () => {
     getMock.mockImplementationOnce((_, cb) => cb(undefined, null));
-    await getTask(redisClientMock as any, aRedisKey)
-      .fold(
-        () => fail(),
-        maybeResult => expect(isNone(maybeResult)).toBeTruthy()
-      )
-      .run();
+    const value = pipe(
+      getTask(redisClientMock as any, aRedisKey),
+      TE.fold(fail, T.of)
+    )();
+    expect(O.isNone(await value)).toBeTruthy();
   });
 
   it("should return an error if redis get value fails", async () => {
     getMock.mockImplementationOnce((_, cb) =>
       cb(new Error("Cannot get value"), null)
     );
-    await getTask(redisClientMock as any, aRedisKey)
-      .fold(
-        _ => expect(_).toBeDefined(),
-        () => fail()
-      )
-      .run();
+    const res = pipe(
+      getTask(redisClientMock as any, aRedisKey),
+      TE.fold(T.of, fail)
+    )();
+    expect(res).toBeDefined();
   });
 });
 
 describe("existsTask", () => {
   it("should return true if key exists in redis", async () => {
-    await existsKeyTask(redisClientMock as any, aRedisKey)
-      .fold(
-        () => fail(),
-        exists => expect(exists).toBeTruthy()
-      )
-      .run();
+    const exists = await pipe(
+      existsKeyTask(redisClientMock as any, aRedisKey),
+      TE.fold(fail, T.of)
+    )();
+    expect(exists).toBeTruthy();
   });
 
   it("should return false if key does not exists in redis", async () => {
     existsMock.mockImplementationOnce((_, cb) => cb(null, 0));
-    await existsKeyTask(redisClientMock as any, aRedisKey)
-      .fold(
-        () => fail(),
-        exists => expect(exists).toBeFalsy()
-      )
-      .run();
+    const exists = await pipe(
+      existsKeyTask(redisClientMock as any, aRedisKey),
+      TE.fold(fail, T.of)
+    )();
+    expect(exists).toBeFalsy();
   });
 
   it("should return an error if redis exists fails", async () => {
     existsMock.mockImplementationOnce((_, cb) =>
       cb(new Error("Cannot recognize exists on redis"), null)
     );
-    await existsKeyTask(redisClientMock as any, aRedisKey)
-      .fold(
-        _ => expect(_).toBeDefined(),
-        () => fail()
-      )
-      .run();
+    const res = await pipe(
+      existsKeyTask(redisClientMock as any, aRedisKey),
+      TE.fold(T.of, fail)
+    )();
+    expect(res).toBeDefined();
   });
 });

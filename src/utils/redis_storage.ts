e0c6080a0c908462d45eb119f008e86b68c882e7
LoFrance <lorenzo.franceschini@pagopa.it>
2022-04-23 10:25:39 +0200

    refactor:utils/redis_storage.ts

--- src/utils/redis_storage.ts
+++ src/utils/redis_storage.ts
@@ -1,8 +1,8 @@
-import { Either, isLeft, left, right, toError } from "fp-ts/lib/Either";
 import { fromNullable, Option } from "fp-ts/lib/Option";
-import { fromEither, TaskEither, tryCatch } from "fp-ts/lib/TaskEither";
 import { RedisClient } from "redis";
-
+import { pipe } from "fp-ts/lib/function";
+import * as E from "fp-ts/lib/Either";
+import * as TE from "fp-ts/lib/TaskEither";
 /**
  * Parse a Redis given string reply.
  *
@@ -12,12 +12,12 @@ export const givenStringReply = (
   err: Error | null,
   reply: string | undefined,
   message: string
-): Either<Error, boolean> => {
+): E.Either<Error, boolean> => {
   if (err) {
-    return left<Error, boolean>(err);
+    return E.left<Error, boolean>(err);
   }
 
-  return right<Error, boolean>(reply === message);
+  return E.right<Error, boolean>(reply === message);
 };
 
 /**
@@ -28,12 +28,12 @@ export const givenStringReply = (
 export const singleStringReply = (
   err: Error | null,
   reply: "OK" | undefined
-): Either<Error, boolean> => {
+): E.Either<Error, boolean> => {
   if (err) {
-    return left<Error, boolean>(err);
+    return E.left<Error, boolean>(err);
   }
 
-  return right<Error, boolean>(reply === "OK");
+  return E.right<Error, boolean>(reply === "OK");
 };
 
 /**
@@ -44,11 +44,11 @@ export const singleStringReply = (
 export const singleValueReply = (
   err: Error | null,
   reply: string | null
-): Either<Error, Option<string>> => {
+): E.Either<Error, Option<string>> => {
   if (err) {
-    return left<Error, Option<string>>(err);
+    return E.left<Error, Option<string>>(err);
   }
-  return right<Error, Option<string>>(fromNullable(reply));
+  return E.right<Error, Option<string>>(fromNullable(reply));
 };
 
 /**
@@ -60,27 +60,27 @@ export const integerReply = (
   err: Error | null,
   reply: unknown,
   expectedReply?: number
-): Either<Error, boolean> => {
+): E.Either<Error, boolean> => {
   if (err) {
-    return left<Error, boolean>(err);
+    return E.left<Error, boolean>(err);
   }
   if (expectedReply !== undefined && expectedReply !== reply) {
-    return right<Error, boolean>(false);
+    return E.right<Error, boolean>(false);
   }
-  return right<Error, boolean>(typeof reply === "number");
+  return E.right<Error, boolean>(typeof reply === "number");
 };
 
 export const falsyResponseToError = (
-  response: Either<Error, boolean>,
+  response: E.Either<Error, boolean>,
   error: Error
-): Either<Error, true> => {
-  if (isLeft(response)) {
-    return left(response.value);
+): E.Either<Error, true> => {
+  if (E.isLeft(response)) {
+    return E.left(response.left);
   } else {
-    if (response.value) {
-      return right(true);
+    if (response.right) {
+      return E.right(true);
     }
-    return left(error);
+    return E.left(error);
   }
 };
 
@@ -90,16 +90,45 @@ export const setWithExpirationTask = (
   value: string,
   expirationInSeconds: number,
   errorMsg?: string
-): TaskEither<Error, true> =>
-  tryCatch(
-    () =>
-      new Promise<Either<Error, true>>(resolve =>
-        redisClient.set(
-          key,
-          value,
-          "EX",
-          expirationInSeconds,
-          (err, response) =>
+): TE.TaskEither<Error, true> =>
+  pipe(
+    TE.tryCatch(
+      () =>
+        new Promise<E.Either<Error, true>>((resolve) =>
+          redisClient.set(
+            key,
+            value,
+            "EX",
+            expirationInSeconds,
+            (err, response) =>
+              resolve(
+                falsyResponseToError(
+                  singleStringReply(err, response),
+                  new Error(
+                    errorMsg
+                      ? errorMsg
+                      : "Error setting key value pair on redis"
+                  )
+                )
+              )
+          )
+        ),
+      E.toError
+    ),
+    TE.chain(TE.fromEither)
+  );
+
+export const setTask = (
+  redisClient: RedisClient,
+  key: string,
+  value: string,
+  errorMsg?: string
+): TE.TaskEither<Error, true> =>
+  pipe(
+    TE.tryCatch(
+      () =>
+        new Promise<E.Either<Error, true>>((resolve) =>
+          redisClient.set(key, value, (err, response) =>
             resolve(
               falsyResponseToError(
                 singleStringReply(err, response),
@@ -108,90 +137,83 @@ export const setWithExpirationTask = (
                 )
               )
             )
-        )
-      ),
-    toError
-  ).chain(fromEither);
+          )
+        ),
+      E.toError
+    ),
+    TE.chain(TE.fromEither)
+  );
 
-export const setTask = (
-  redisClient: RedisClient,
-  key: string,
-  value: string,
-  errorMsg?: string
-): TaskEither<Error, true> =>
-  tryCatch(
-    () =>
-      new Promise<Either<Error, true>>(resolve =>
-        redisClient.set(key, value, (err, response) =>
-          resolve(
-            falsyResponseToError(
-              singleStringReply(err, response),
-              new Error(
-                errorMsg ? errorMsg : "Error setting key value pair on redis"
+export const deleteTask = (redisClient: RedisClient, key: string) =>
+  pipe(
+    TE.tryCatch(
+      () =>
+        new Promise<E.Either<Error, boolean>>((resolve) =>
+          redisClient.del(key, (err, response) =>
+            resolve(
+              falsyResponseToError(
+                integerReply(err, response),
+                new Error("Error deleting key value pair on redis")
               )
             )
           )
-        )
-      ),
-    toError
-  ).chain(fromEither);
-
-export const deleteTask = (redisClient: RedisClient, key: string) =>
-  tryCatch(
-    () =>
-      new Promise<Either<Error, boolean>>(resolve =>
-        redisClient.del(key, (err, response) =>
-          resolve(
-            falsyResponseToError(
-              integerReply(err, response),
-              new Error("Error deleting key value pair on redis")
-            )
-          )
-        )
-      ),
-    toError
-  ).chain(fromEither);
+        ),
+      E.toError
+    ),
+    TE.chain(TE.fromEither)
+  );
 
 export const getTask = (
   redisClient: RedisClient,
   key: string
-): TaskEither<Error, Option<string>> =>
-  tryCatch(
-    () =>
-      new Promise<Either<Error, Option<string>>>(resolve =>
-        redisClient.get(key, (err, response) =>
-          resolve(singleValueReply(err, response))
-        )
-      ),
-    toError
-  ).chain(fromEither);
+): TE.TaskEither<Error, Option<string>> =>
+  pipe(
+    TE.tryCatch(
+      () =>
+        new Promise<E.Either<Error, Option<string>>>((resolve) =>
+          redisClient.get(key, (err, response) =>
+            resolve(singleValueReply(err, response))
+          )
+        ),
+      E.toError
+    ),
+    TE.chain(TE.fromEither)
+  );
 
 export const existsKeyTask = (
   redisClient: RedisClient,
   key: string
-): TaskEither<Error, boolean> =>
-  tryCatch(
-    () =>
-      new Promise<Either<Error, boolean>>(resolve =>
-        redisClient.exists(key, (err, response) =>
-          resolve(integerReply(err, response, 1))
-        )
-      ),
-    toError
-  ).chain(fromEither);
+): TE.TaskEither<Error, boolean> =>
+  pipe(
+    TE.tryCatch(
+      () =>
+        new Promise<E.Either<Error, boolean>>((resolve) =>
+          redisClient.exists(key, (err, response) =>
+            resolve(integerReply(err, response, 1))
+          )
+        ),
+      E.toError
+    ),
+    TE.chain(TE.fromEither)
+  );
 
-export const pingTask = (redisClient: RedisClient): TaskEither<Error, true> =>
-  tryCatch(
-    () =>
-      new Promise<Either<Error, true>>(resolve =>
-        redisClient.ping("ping message", (err, response) =>
-          resolve(
-            falsyResponseToError(
-              givenStringReply(err, response, "ping message"),
-              new Error("Error while pinging redis")
+export const pingTask = (
+  redisClient: RedisClient
+): TE.TaskEither<Error, true> =>
+  pipe(
+    TE.tryCatch(
+      () =>
+        new Promise<E.Either<Error, true>>((resolve) =>
+          redisClient.ping("ping message", (err, response) =>
+            resolve(
+              falsyResponseToError(
+                givenStringReply(err, response, "ping message"),
+                new Error("Error while pinging redis")
+              )
             )
           )
-        )
-      ),
-    toError
-  ).chain(fromEither);
+        ),
+      E.toError
+    ),
+    TE.chain(TE.fromEither)
+  );

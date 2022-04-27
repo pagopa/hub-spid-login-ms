ff1b115814f2f90243a471ba30bebfad4d89f7b6
LoFrance <lorenzo.franceschini@pagopa.it>
2022-04-27 17:17:25 +0200

    refactor: src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { getConfigOrThrow } from "./utils/config";
 
 const config = getConfigOrThrow();
 const appInsights = initAppInsights(config.APPINSIGHTS_INSTRUMENTATIONKEY, {
-  disableAppInsights: config.APPINSIGHTS_DISABLED
+  disableAppInsights: config.APPINSIGHTS_DISABLED,
 });
 
 // tslint:disable-next-line: no-object-mutation
@@ -15,12 +15,11 @@ appInsights.context.tags[appInsights.context.keys.cloudRole] =
 // tslint:disable-next-line: no-let
 let server: http.Server;
 
-createAppTask
-  .run()
-  .then(app => {
+createAppTask()
+  .then((app) => {
     server = http.createServer(app).listen(config.SERVER_PORT);
     // tslint:disable-next-line: no-console
     console.log(`Server listening at port ${config.SERVER_PORT}`);
   })
   // tslint:disable-next-line: no-console
-  .catch(e => console.error("Application error: ", e));
+  .catch((e) => console.error("Application error: ", e));

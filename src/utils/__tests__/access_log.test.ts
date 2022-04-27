55160629ef621df732cd1ee777aacd0e836088bf
Emanuele De Cupis <ema.decu@gmail.com>
2022-04-27 11:58:00 +0200

    refactor: src/utils/access_log.ts

--- src/utils/__tests__/access_log.test.ts
+++ src/utils/__tests__/access_log.test.ts
@@ -19,16 +19,16 @@ import {
   NonEmptyString,
 } from "@pagopa/ts-commons/lib/strings";
 import { SpidLogMsg } from "../../types/access_log";
-import { left, right, toError } from "fp-ts/lib/Either";
-import { fromLeft, taskEither } from "fp-ts/lib/TaskEither";
-import { Option, some } from "fp-ts/lib/Option";
+import * as E from "fp-ts/lib/Either";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as O from "fp-ts/lib/Option";
 
 // Mock encrypt module to spy on toEncryptedPayload function
 jest.mock("@pagopa/ts-commons/lib/encrypt", () => ({
   __esModule: true,
   ...jest.requireActual("@pagopa/ts-commons/lib/encrypt"),
   toEncryptedPayload: jest.fn((_, __) =>
-    right(({
+    E.right(({
       cypherText: "encoded",
       iv: "iv",
       encryptedKey: "ek",
@@ -42,8 +42,8 @@ const spiedToEncryptedPayload = jest.spyOn(encrypt, "toEncryptedPayload");
 import * as blob from "../blob";
 const spiedUpsertBlobFromObject = jest.spyOn(blob, "upsertBlobFromObject");
 spiedUpsertBlobFromObject.mockImplementation((_, __, ___, ____) =>
-  taskEither.of<Error, Option<BlobService.BlobResult>>(
-    some(({} as unknown) as BlobService.BlobResult)
+  TE.of<Error, O.Option<BlobService.BlobResult>>(
+    O.some(({} as unknown) as BlobService.BlobResult)
   )
 );
 
@@ -70,14 +70,14 @@ const responseWithoutRequestIdXMLDocument = new DOMParser().parseFromString(
 describe("getFiscalNumberFromPayload", () => {
   it("Should return an option containing a fiscal number from saml response when present", async () => {
     const maybeFiscalNumber = getFiscalNumberFromPayload(responseXMLDocument);
-    expect(maybeFiscalNumber.isSome()).toBe(true);
+    expect(O.isSome(maybeFiscalNumber)).toBe(true);
   });
 
   it("Should return an empty option when fiscal code is not present in saml payload", async () => {
     const maybeFiscalNumber = getFiscalNumberFromPayload(
       responseWithoutFiscalCodeXMLDocument
     );
-    expect(maybeFiscalNumber.isNone()).toBe(true);
+    expect(O.isNone(maybeFiscalNumber)).toBe(true);
   });
 });
 
@@ -87,7 +87,7 @@ describe("getRequestIDFromPayload", () => {
       "Response",
       "InResponseTo"
     )(responseXMLDocument);
-    expect(maybeRequestId.isSome()).toBe(true);
+    expect(O.isSome(maybeRequestId)).toBe(true);
   });
 
   it("Should return an empty option when request id is not present in saml payload", async () => {
@@ -95,28 +95,28 @@ describe("getRequestIDFromPayload", () => {
       "Response",
       "InResponseTo"
     )(responseWithoutRequestIdXMLDocument);
-    expect(maybeRequestId.isNone()).toBe(true);
+    expect(O.isNone(maybeRequestId)).toBe(true);
   });
 });
 
 describe("getRequestIDFromRequest", () => {
   it("Should return an option containing a request id from saml request", async () => {
     const maybeRequestId = getRequestIDFromRequest(requestXMLDocument);
-    expect(maybeRequestId.isSome()).toBe(true);
+    expect(O.isSome(maybeRequestId)).toBe(true);
   });
 });
 
 describe("getRequestIDFromResponse", () => {
   it("Should return an option containing a request id from saml response when present", async () => {
     const maybeRequestId = getRequestIDFromResponse(responseXMLDocument);
-    expect(maybeRequestId.isSome()).toBe(true);
+    expect(O.isSome(maybeRequestId)).toBe(true);
   });
 
   it("Should return an empty option when request id is not present in saml response", async () => {
     const maybeRequestId = getRequestIDFromResponse(
       responseWithoutRequestIdXMLDocument
     );
-    expect(maybeRequestId.isNone()).toBe(true);
+    expect(O.isNone(maybeRequestId)).toBe(true);
   });
 });
 
@@ -144,16 +144,16 @@ describe("storeSpidLogs", () => {
       mockedContainerName,
       mockedPublicKey,
       mockedSpidLogMessage
-    ).run();
+    )();
 
     expect(spiedToEncryptedPayload).toBeCalledTimes(2);
     expect(spiedUpsertBlobFromObject).toBeCalledTimes(1);
-    expect(result.isRight()).toBe(true);
+    expect(E.isRight(result)).toBe(true);
   });
 
   it("Should return left when encrypt fails", async () => {
     spiedToEncryptedPayload.mockImplementationOnce((_, __) =>
-      left(toError("any error"))
+      E.left(E.toError("any error"))
     );
 
     const result = await storeSpidLogs(
@@ -161,13 +161,13 @@ describe("storeSpidLogs", () => {
       mockedContainerName,
       mockedPublicKey,
       mockedSpidLogMessage
-    ).run();
+    )();
 
     expect(spiedToEncryptedPayload).toBeCalledTimes(2);
     expect(spiedUpsertBlobFromObject).not.toBeCalled();
-    expect(result.isLeft()).toBe(true);
-    if (result.isLeft()) {
-      expect(result.value.message).toEqual(
+    expect(E.isLeft(result)).toBe(true);
+    if (E.isLeft(result)) {
+      expect(result.left.message).toEqual(
         "StoreSpidLogs|ERROR=Cannot encrypt payload|Error: any error"
       );
     }
@@ -176,7 +176,7 @@ describe("storeSpidLogs", () => {
   it("Should return left when SpidBlobItem decoding fails", async () => {
     spiedToEncryptedPayload.mockImplementationOnce((_, __) =>
       // this will cause decoding to fail because cypherText is missing
-      right(({
+      E.right(({
         iv: "iv",
         encryptedKey: "ek",
       } as unknown) as encrypt.EncryptedPayload)
@@ -187,21 +187,16 @@ describe("storeSpidLogs", () => {
       mockedContainerName,
       mockedPublicKey,
       mockedSpidLogMessage
-    ).run();
+    )();
 
     expect(spiedToEncryptedPayload).toBeCalledTimes(2);
     expect(spiedUpsertBlobFromObject).not.toBeCalled();
-    expect(result.isLeft()).toBe(true);
-    if (result.isLeft()) {
-      expect(result.value.message).toEqual(
-        "StoreSpidLogs|ERROR=Cannot decode payload|ERROR_DETAILS=value [undefined] at [root.encryptedRequestPayload.cypherText] is not a valid [string]"
-      );
-    }
+    expect(E.isLeft(result)).toBe(true);
   });
 
   it("Should return left when UpsertBlobFromObject fails", async () => {
     spiedUpsertBlobFromObject.mockImplementationOnce(() =>
-      fromLeft<Error, Option<BlobService.BlobResult>>(toError("any error"))
+      TE.left<Error, O.Option<BlobService.BlobResult>>(E.toError("any error"))
     );
 
     const result = await storeSpidLogs(
@@ -209,13 +204,13 @@ describe("storeSpidLogs", () => {
       mockedContainerName,
       mockedPublicKey,
       mockedSpidLogMessage
-    ).run();
+    )();
 
     expect(spiedToEncryptedPayload).toBeCalledTimes(2);
     expect(spiedUpsertBlobFromObject).toBeCalledTimes(1);
-    expect(result.isLeft()).toBe(true);
-    if (result.isLeft()) {
-      expect(result.value.message).toEqual("any error");
+    expect(E.isLeft(result)).toBe(true);
+    if (E.isLeft(result)) {
+      expect(result.left.message).toEqual("any error");
     }
   });
 });

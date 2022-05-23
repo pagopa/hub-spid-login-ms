027b28f31ff65ffeed332e9518a8ad5532d9e1b4
LoFrance <lorenzo.franceschini@pagopa.it>
2022-05-23 12:01:09 +0200

    Add status 429 handled as Error Internal

--- src/utils/user_registry.ts
+++ src/utils/user_registry.ts
@@ -3,6 +3,7 @@ import {
   IResponseErrorInternal,
   IResponseErrorValidation,
   ResponseErrorForbiddenNotAuthorized,
+  ResponseErrorInternal,
   ResponseErrorValidation,
 } from "@pagopa/ts-commons/lib/responses";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
@@ -10,51 +11,50 @@ import { toError } from "fp-ts/lib/Either";
 import { Option, some } from "fp-ts/lib/Option";
 import { SaveUserDto } from "../../generated/pdv-userregistry-api/SaveUserDto";
 import { UserId } from "../../generated/pdv-userregistry-api/UserId";
-import { flow, pipe } from "fp-ts/lib/function";
+import { pipe } from "fp-ts/lib/function";
 
+import * as E from "fp-ts/lib/Either";
 import * as TE from "fp-ts/lib/TaskEither";
 import { PersonalDatavaultAPIClient } from "../clients/pdv_client";
 import { toResponseErrorInternal } from "./conversions";
+
+type ErrorResponses =
+  | IResponseErrorForbiddenNotAuthorized
+  | IResponseErrorInternal
+  | IResponseErrorValidation;
 
 export const blurUser = (
   pdvClient: ReturnType<PersonalDatavaultAPIClient>,
   user: SaveUserDto,
   subscriptionKey: NonEmptyString
-): TE.TaskEither<
-  | IResponseErrorInternal
-  | IResponseErrorValidation
-  | IResponseErrorForbiddenNotAuthorized,
-  Option<Pick<UserId, "id">>
-> =>
+): TE.TaskEither<ErrorResponses, Option<Pick<UserId, "id">>> =>
   pipe(
     TE.tryCatch(
       () =>
         pdvClient.saveUsingPATCH({
           api_key: subscriptionKey,
-          body: user,
+          saveUserDto: user,
         }),
       toError
     ),
     TE.mapLeft((error) => toResponseErrorInternal(toError(error))),
-    TE.chainW(
-      flow(
-        TE.fromEither,
-        TE.mapLeft(() =>
-          ResponseErrorValidation("Bad Input", "Error creating the user")
-        )
+    TE.chainEitherKW(
+      E.mapLeft(() =>
+        ResponseErrorValidation("Bad Input", "Error creating the user")
       )
     ),
     TE.chainW((res) => {
       switch (res.status) {
-        case 200: {
+        case 200:
           return TE.of(some({ id: res.value.id }));
-        }
-        case 401: {
-          return TE.left(ResponseErrorForbiddenNotAuthorized);
-        }
-        case 403: {
-          return TE.left(ResponseErrorForbiddenNotAuthorized);
-        }
+        case 401:
+          return TE.left<ErrorResponses>(ResponseErrorForbiddenNotAuthorized);
+        case 403:
+          return TE.left<ErrorResponses>(ResponseErrorForbiddenNotAuthorized);
+        case 429:
+          return TE.left<ErrorResponses>(
+            ResponseErrorInternal("Error internal")
+          );
       }
     })
   );

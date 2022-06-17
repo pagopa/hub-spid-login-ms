5bc11a5227967a61cdaff3de4d0bd255ccf3dfcb
Emanuele De Cupis <ema.decu@gmail.com>
2022-06-17 09:56:18 +0200

    [#hsl-6] [#hsl-4] avoid same names for containers

--- e2e/scenarios/basic/config.ts
+++ e2e/scenarios/basic/config.ts
@@ -1,6 +1,7 @@
+import { envFlag } from "../../utils/misc";
+
 export const host = "http://localhost:9090";
 export const testEntityID = "xx_validator";
-export const showBrowser =
-  process.env.DEBUG === "1" || process.env.DEBUG === "true";
+export const showBrowser = envFlag(process.env.DEBUG);
 // see users.json in conf-testenv directory defined for the current spid-testenv instance
 export const testCredentials = ["ada", "password123"];

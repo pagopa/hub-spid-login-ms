0f82a56a4b2bd80a9bd79a8d22a092f7435d8bd6
Emanuele De Cupis <ema.decu@gmail.com>
2022-06-22 17:21:00 +0200

    [#hsl-6] add e2e tests for aws s3

---
+++ e2e/scenarios/with-aws-s3/config.ts
@@ -0,0 +1,18 @@
+import { envFlag } from "../../utils/misc";
+
+// read all users and take any of them
+const [user1] = require("./res/spidsamlcheck-conf/spid_users.json");
+
+export const host = "http://localhost:9092";
+export const testEntityID = "xx_validator";
+export const showBrowser = envFlag(process.env.DEBUG);
+
+export const testCredentials = [
+  user1.username,
+  user1.password,
+  user1.fiscalNumber.split("-")[1],
+];
+export const spidLogStorage = {
+  containerName: "spidassertions",
+  connectionString: "minioadmin;minioadmin;http://localhost:10000",
+};

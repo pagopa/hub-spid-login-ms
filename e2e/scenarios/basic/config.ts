543521a54221dee05438b47135f0299e53dfb829
Michael Disaro <michael.disaro@gmail.com>
2023-04-28 11:16:51 +0200

    fixed decoder

--- e2e/scenarios/basic/config.ts
+++ e2e/scenarios/basic/config.ts
@@ -1,6 +1,6 @@
 import { envFlag } from "../../utils/misc";
 
-export const host = "http://localhost:9090";
+export const host = "http://127.0.0.1:9090";
 export const testEntityID = "xx_validator";
 export const showBrowser = envFlag(process.env.DEBUG);
 // see users.json in conf-testenv directory defined for the current spid-testenv instance

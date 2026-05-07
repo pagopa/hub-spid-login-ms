0ad94f564e0485ad85f3760552167cc0fdf846db
Daniele Manni <dan.manni@gmail.com>
2026-05-07 17:23:54 +0200

    Add Terraform configuration files for production environment setup

---
+++ infra/bootstrapper/prod/local.tf
@@ -0,0 +1,59 @@
+locals {
+  prefix          = "io"
+  env_short       = "p"
+  location        = "italynorth"
+  domain          = "plt"
+  instance_number = "04"
+
+  adgroups = {
+    admins_name    = "io-p-adgroup-platform-admins"
+    devs_name      = "io-p-adgroup-platform-developers"
+    externals_name = "io-p-adgroup-platform-externals"
+  }
+
+  runner = {
+    cae_name                = "${local.prefix}-${local.env_short}-itn-github-runner-cae-01"
+    cae_resource_group_name = "${local.prefix}-${local.env_short}-itn-github-runner-rg-01"
+    secret = {
+      kv_name                = "${local.prefix}-${local.env_short}-kv-common"
+      kv_resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
+    }
+  }
+
+  apim = {
+    name                = "${local.prefix}-${local.env_short}-itn-apim-01"
+    resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
+  }
+
+  vnet = {
+    name                = "${local.prefix}-${local.env_short}-itn-common-vnet-01"
+    resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
+  }
+
+  dns_zones = {
+    resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
+  }
+
+  tf_storage_account = {
+    name                = "iopitntfst001"
+    resource_group_name = "terraform-state-rg"
+  }
+
+  repository = {
+    name = "hub-spid-login-ms"
+  }
+
+  key_vault = {
+    name                = "${local.prefix}-${local.env_short}-kv-common"
+    resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
+  }
+
+  tags = {
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    BusinessUnit   = "App IO"
+    ManagementTeam = "IO Platform"
+    CostCenter     = "TS000 - Tecnologia e Servizi"
+    Source         = "https://github.com/pagopa/hub-spid-login-ms/blob/main/infra/bootstrapper/prod"
+  }
+}
